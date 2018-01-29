a49398dfa4f62749059c83209612e82b2cffb1bd
Wenjie Wang <wenjie.2.wang@uconn.edu>
2018-01-28 19:47:34 -0500

    add example apps

---
+++ examples/DT/app.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(DT)
+
+ui <- fluidPage(
+
+    titlePanel("a simple Shiny app with DT"),
+
+    fluidRow(
+        column(width = 4,
+               selectInput("cyl", "Cylinders",
+                           choices = sort(unique(mtcars$cyl)),
+                           selected = 4)
+               )
+    ),
+
+    fluidRow(
+        column(width = 12,
+               dataTableOutput("dt")
+               )
+    )
+)
+
+server <- function(input, output) {
+
+    dataInput <- reactive({
+        subset(mtcars, cyl == input$cyl, select = - cyl)
+    })
+
+    output$dt <- renderDataTable({
+        dataInput()
+    })
+
+}
+
+shinyApp(ui, server)
